0a6a40496f100dd45dafeb3c9ecf008551882bab
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-24 21:36:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,4 +9,38 @@ Comentário
 de 
 bloco
 */
+
+$nome='Daniel';//variavel
+
+echo "Meu nome é $nome";
+//Comentário de linha
+#Comentário de linha 
+/*
+Comentário
+de 
+bloco
+*/
+
+//DOCUMENTAÇÃO DO PHP FICA EM:https://www.php.net/manual/en/function.echo
+
+//Interpolação Linear
+
+for( $i = 0 ; $i < 10 ; $i++ ){
+
+    echo "Daniel <br>";
+}
+
+$i = 0;
+while($i < 10){
+
+    echo "Daniel";
+    $i++;
+}
+
+//DO WHILE
+do{
+    echo "Daniel";
+    $i++
+}
+//FIM LOOPINGS
 ?>
