9e87e67b9b9c8cadd4cb6227a91e51c4e96aee12
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-08-24 22:09:25 -0300

    Operadores, loopings e condicionais.

--- index.php
+++ index.php
@@ -1,12 +1,77 @@
 <?php
-$nome='Daniel';
+$nome = 'Olá Daniel!';
+echo "$nome Numero 1 <br>";
 
-echo "Olá $nome";
-//Comentário de linha
-#Comentário de linha 
-/*
-Comentário
-de 
+
+//comentario de linha
+#comentario de linha 
+/* comentario
+em
 bloco
 */
 
+//documentação de php fica em: https://www.php.net
+
+//LOOPINGS
+
+//FOR
+for ($i = 2; $i <= 10; $i++) {
+    echo "$nome Daniel $i <br>"; 
+}
+echo "<br>";
+
+//WHILE
+$i = 11;
+while ($i <= 20) {
+    echo "$nome Daniel $i <br>";
+    $i++; }
+
+//DO WHILE
+echo "<br>";
+$i = 21;
+do{
+    echo "$nome Daniel $i <br>";
+    $i++;
+} while ($i <= 30);
+
+//FIM LOOPINGS
+
+////////////////////////////////////////////////
+//CONDICIONAIS
+
+//IF
+echo "<br>";
+
+if (5 > 10) {
+    echo "5 é maior que 10";
+} else {
+    echo "5 não é maior que 10";
+}
+
+echo "<br>";
+
+$d = 2;
+switch ($d) {
+    case 0:
+        echo "Domingo";
+        break;
+    case 1:
+        echo "Segunda";
+        break;
+    case 2:
+        echo "Terça";
+        break;
+    case 3:
+        echo "Quarta";
+        break;
+    case 4:
+        echo "Quinta";
+        break;
+    case 5:
+        echo "Sexta";
+        break;
+    case 6:
+        echo "Sabado";
+        break;    
+}
+//FIM CONDICIONAIS
