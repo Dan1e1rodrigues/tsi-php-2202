23c2ed8cd6096edbcafeb2eedb53ade1ef02d1ed
Daniel Rodrigues <rodriguesdeaguiardaniel@gmail.com>
2022-09-21 22:27:35 -0300

    hora

--- formulario/bancodedados/conecta.php
+++ formulario/bancodedados/conecta.php
@@ -1,9 +1,11 @@
 <?php
+//dados para nos conectarmos ao SGBD
 
 $dsn = 'mysql:dbname=senac;host=localhost;port=3307';
 $user = 'root';
 $pass = '';
 
+//Conectamos ao SGBD
 $bd = new PDO($dsn,$user, $pass);
 
 var_dump($bd);
